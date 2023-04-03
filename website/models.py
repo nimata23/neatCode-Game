1092601f77e02271c72611a11ed336a14b8973d1
nimata23 <nimata23@colby.edu>
2023-04-03 16:15:58 -0400

    creates the user model for the database. Needs feedback from naser on session score but should work for now, untested at this moment

---
+++ website/models.py
@@ -0,0 +1,10 @@
+from . import db
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    id = id.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    highscore = db.Column(db.Integer)
+    session_score = db.Column(db.Integer)
