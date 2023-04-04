242273567bf275fd8ea9f6f546ccf54e5c9748ae
Jonathan Sosa Uranga <Jsosau24@colby.edu>
2023-04-04 11:31:13 -0400

    changed models.py

--- website/models.py
+++ website/models.py
@@ -1,10 +1,17 @@
+# imports
+from sqlalchemy.sql import func
+from flask_sqlalchemy import SQLAlchemy
 from . import db
-from flask_login import UserMixin
 
+# leaderboard data class
+class Leaderboard(db.Model):
+    username = db.Column(db.String(30), unique=True)
 
-class User(db.Model, UserMixin):
+# user data class
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+
+    username = db.Column(db.String(30), unique=True)
+    password = db.Column(db.String(30))
     highscore = db.Column(db.Integer)
-    session_score = db.Column(db.Integer)
+    current_score = db.Column(db.Integer)
