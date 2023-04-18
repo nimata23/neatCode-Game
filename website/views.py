a417b8c23b6d656a78d13bc6c5c8f885149ec4fa
nimata23 <nimata23@colby.edu>
2023-04-18 02:30:06 -0400

    working on game view. testing predict, predict has issue finding the file. looking for workaround

--- website/views.py
+++ website/views.py
@@ -4,7 +4,7 @@ from . import db
 from flask_login import login_required, current_user
 from .code2vec import code2vec, interactive_predict
 import random
-from . import java_samples
+import os
 
 
 views = Blueprint('views', __name__)
@@ -30,8 +30,9 @@ def game():
     model, config = code2vec.c2v()
 
     #as default select the first file of the first level
-    file_base = "website/java_samples/level1/file"
-    filename = file_base + '1' + ".java"
+    base_path = "website/java_samples/"
+    file_base = base_path + 'level1/file'
+    filename = file_base + '1.java'
     code_block = read_file(filename)
 
     #print to test
@@ -44,22 +45,22 @@ def game():
         #get input from forms (level selection, name predition, or move to next example)
         level_select = request.form.get('level')
         next_file = request.form.get('next')
-        predict = request.form.get('prediction')
+        predict = request.form.get('answer')
 
         #if changing the level, change the base of the file names and update the displayed information
         if level_select:
             if level_select == 1:
-                file_base = "java_samples/level1/file"
-                filename = file_base + '1' + ".java"
-                lines = read_file(filename)
+                file_base = base_path + "level1/file"
+                filename = file_base + "1.java"
+                code_block = read_file(filename1)
             elif level_select == 2:
-                file_base = "java_samples/level2/file"
+                file_base = base_path + "level2/file"
                 filename = file_base + '1'+ ".java"
-                lines = read_file(filename)
+                code_block = read_file(filename)
             elif level_select == 3:
-                file_base = "java_samples/level3/file"
+                file_base = base_path + "level3/file"
                 filename = file_base + '1'+ ".java"
-                lines = read_file(filename)
+                code_block = read_file(filename)
         
         #check if you are moving to the next example
         elif next_file:
@@ -67,23 +68,24 @@ def game():
             num = str(random.randint(2, 10))
             #add the number to the filename and read the file
             filename = filebase + num +".java"
-            lines = read_file(filename)
+            lines = read_file(filepath)
 
         #check if you are predicting the method name
-        elif predict and lives > 0:
+        if predict != "":
             #if there is prediction, create the predictor and pass in the file you are predicting 
             #the name of
-            predictor = InteractivePredictor(config, model)
+            print("calling predict")
+            predictor = interactive_predict.InteractivePredictor(config, model)
             predictor.predict(filename)
-            lives_left  = lives_left - 1
+            #lives_left  = lives_left - 1
     
         #if the user is out of lives
         elif predict and lives <= 0:
             flash("no lives left, please click next")
     return render_template("game.html", user=current_user, code=code_block, active_page='game.html')
 
-def read_file(filename):
-    file1 = open(filename, 'r')
+def read_file(filepath):
+    file1 = open(filepath, 'r')
     data = file1.readlines()
     file1.close()
     return data
